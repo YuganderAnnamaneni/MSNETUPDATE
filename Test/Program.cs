f040ddd2542005ba45402701fc5160764a65fc2c
Yugander_Annamaneni <Yugander_Annamaneni@EPINHYDW0276>
2017-04-14 14:46:51 +0530

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person;
+            person.FirstName = "Yugander";
+            person.LastName = "Annamaneni";
+            person.Age = 100;
+
+            Console.Write(person.FirstName + "\n");
+            Console.Write(person.LastName + "\n");
+            Console.Write(person.Age + "\n");
+
+            Object obj = person;//boxing value type to reference type.
+            person = (Person)obj;//unboxing object type to value type(struct).
+
+            Console.Write(person.FirstName + "\n");
+
+            Console.ReadLine();
+        }
+    }
+
+    public struct Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+    }
+}
